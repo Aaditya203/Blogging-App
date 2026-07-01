8746c5c8d54e3174e4910e915f0802e08fe17b98
Aditya Sharma <adityas20032005@gmail.com>
2026-07-01 16:01:12 +0530

    Prepare Frontend Deployment

--- frontend/src/lib/axios.ts
+++ frontend/src/lib/axios.ts
@@ -3,7 +3,8 @@
 import axios from "axios";
 
 const api = axios.create({
-  baseURL: "http://localhost:3000/api/v1",
+  baseURL: import.meta.env.VITE_BACKEND_URL,
+  withCredentials:true
 });
 
 api.interceptors.request.use((config) => {
